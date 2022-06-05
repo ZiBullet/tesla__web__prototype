3ae3cc91025fe3b748679202d3c8e00dbb42388f
ZiBullet <zamolidinziadulaev@gmail.com>
2022-06-05 20:43:32 +0500

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const startBtn = document.querySelector('.starter')
+const door = document.querySelector('.door')
 const teslaCar = document.querySelector('.screen__tesla')
-console.log(teslaCar);
+const ch = document.querySelector('.ch')
+const car = document.querySelector('.car')
+const secImg = document.querySelector('.secondImg')
+const secondDoor = document.querySelector('.door2')
+const thirdImg = document.querySelector('.thirdImg')
 
-startBtn.onclick = () => {
-    teslaCar.classList.add('start')
+door.onclick = () => {
+    teslaCar.classList.remove('ch')
+    car.style.display = 'none'
+    door.style.display = 'none'
+    secImg.style.display = 'block'
+    setTimeout(() => {
+        secImg.style.opacity = '1'
+    }, 200);
+}
+secondDoor.onclick = () => {
+    secImg.classList.add('scale')
+    setTimeout(() => {
+        secImg.style.opacity = '0'
+    }, 200);
+    setTimeout(() => {
+        secImg.style.display = 'none' 
+        secondDoor.style.display = 'none'
+    }, 300);
+    thirdImg.style.display = 'block'
+    setTimeout(() => {
+        thirdImg.style.opacity = '1'
+    }, 400);
 }
